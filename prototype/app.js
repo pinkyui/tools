474bc2201278a93346d0a92d30e944ed4b09b0b9
Muhammad Nur Fuad <illvart.com@gmail.com>
2019-11-15 13:08:24 +0700

    Add options, size and improve the code

--- prototype/app.js
+++ prototype/app.js
@@ -8,10 +8,15 @@
 (function(win, doc) {
   'use strict';
 
-  // Variables
   var arr = [];
 
-  // Delay utils
+  var $ = function(el, selector) {
+    if (typeof el === 'string') {
+      return doc.querySelector(el);
+    } else {
+      return el.querySelector(selector);
+    }
+  };
   var delay = function(fn, wait) {
     var args = arguments;
     var _args = [],
@@ -21,87 +26,107 @@
     }
     return setTimeout.apply(void 0, [fn, +wait || 0].concat(_args));
   };
+  var commify = function(str) {
+    return String(str)
+      .split('')
+      .reverse()
+      .join('')
+      .replace(/(...)(?!$)/g, '$1,')
+      .split('')
+      .reverse()
+      .join('');
+  };
 
   // Tools alert
-  function toolsAlert(id, timeout, content, margin) {
-    var target = doc.querySelector('#' + id + '-alert');
+  function toolsAlert(name, timeout, content, margin) {
+    var target = $('#' + name + '-alert');
     target.innerHTML = content;
     target.style.display = 'block';
     target.style.margin = margin || '12px 0px';
     delay(function() {
       target.style.display = 'none';
-      target.style.margin = '0px';
     }, timeout);
   }
 
-  // Tools clear button textarea/input
-  function toolsClear(btn, disable, target1, target2) {
-    var btn = doc.querySelector('#' + btn + '-clear');
-    btn.onclick = function() {
-      target1.value = '';
-      if (target2 ? target2 : null) {
-        target2.value = '';
-      }
-      target1.focus();
-      disable.disabled = false;
-    };
-  }
-
   // Tools undo
-  function toolsUndo(name, disable) {
-    var undo = doc.querySelector('#' + name + '-undo');
-    var input = doc.querySelector('#' + name + '-input');
-    undo.onclick = function() {
-      var last = arr.pop();
-      input.value = last;
-      if (arr.length == 0) {
-        undo.style.display = 'none';
-      }
-      disable.disabled = false;
-    };
+  function toolsUndo(name, submit) {
+    var undo = $('#' + name + '-undo');
+    var input = $('#' + name + '-input');
+    undo.addEventListener(
+      'click',
+      function() {
+        var last = arr.pop();
+        input.value = last;
+        if (arr.length === 0) {
+          undo.style.display = 'none';
+        }
+        submit.disabled = false;
+      },
+      false
+    );
   }
 
-  // Tools API
-  function tools(name, fn, opts) {
-    opts = opts || {};
-    var submit = doc.querySelector('#' + name + '-submit');
-    var undo = doc.querySelector('#' + name + '-undo');
-    var input = doc.querySelector('#' + name + '-input');
-    var alert = doc.querySelector('#' + name + '-alert');
-    submit.onclick = function() {
-      var val = input.value;
-      if (val === '') {
-        toolsAlert(name, 1500, 'Please insert your <b>code</b> first!');
-      }
-      if (!opts.allowEmpty) {
-        if (!val.length) {
-          return;
+  // Tools size calculate original and output
+  function toolsSize(name, original, output) {
+    var size = $('#' + name + '-size');
+    size.style.display = 'block';
+    size.style.margin = '12px 0px';
+    var diff = original.length - output.length;
+    var savings = original.length
+      ? ((100 * diff) / original.length).toFixed(2)
+      : 0;
+    size.innerHTML =
+      'Original size: <strong>' +
+      commify(original.length) +
+      ' bytes</strong>.<br> Output size: <strong>' +
+      commify(output.length) +
+      ' bytes</strong>.<br> Savings: <strong>' +
+      commify(diff) +
+      ' bytes (' +
+      savings +
+      '%)</strong>.</span>';
+  }
+
+  // Tools clear button textarea/input
+  function toolsClear(name, input, submit) {
+    var btn = $('#' + name + '-clear');
+    var size = $('#' + name + '-size');
+    btn.addEventListener(
+      'click',
+      function() {
+        input.value = '';
+        input.focus();
+        submit.disabled = false;
+        if (size) {
+          size.style.display = 'none';
         }
-      }
-      alert.style.display = 'none';
-      try {
-        if (opts.asyncResult) {
-          fn(val, opts.asyncResult);
+      },
+      false
+    );
+  }
+
+  // Tools options for config
+  function toolsOptions(name) {
+    var options = $('#' + name + '-options');
+    var settings = $('#' + name + '-settings');
+    if (!options || !settings) {
+      return;
+    }
+    options.addEventListener(
+      'click',
+      function() {
+        if (options.checked === true) {
+          settings.style.display = 'block';
         } else {
-          var result = fn(val, opts.asyncResult);
-          input.value = result;
+          settings.style.display = 'none';
         }
-      } catch (err) {
-        if (opts.exception) {
-          opts.exception(err);
-        }
-        return;
-      }
-      arr.push(val);
-      submit.disabled = true;
-      undo.style.display = 'block';
-    };
-    toolsUndo(name, submit);
-    toolsClear(name, submit, input);
+      },
+      false
+    );
   }
 
   // Native SmoothScroll
-  var smoothScrollTo = function(el) {
+  var toolsScrollTo = function(el) {
     win.scroll({
       behavior: 'smooth',
       left: 0,
@@ -110,18 +135,72 @@
   };
   var supportsSmoothScroll = 'scrollBehavior' in doc.documentElement.style;
 
-  // Copy to Clipboard
-  doc.querySelector('.tools-copy').onclick = function() {
-    var textarea = doc.querySelector('#tools textarea');
-    textarea.select();
-    if (!doc.execCommand) {
-      return;
-    }
-    doc.execCommand('copy');
-    if (supportsSmoothScroll) {
-      smoothScrollTo(textarea);
-    }
-  };
+  // Tools API
+  function tools(name, fn, options) {
+    options = options || {};
+    var submit = $('#' + name + '-submit');
+    var input = $('#' + name + '-input');
+    var undo = $('#' + name + '-undo');
+    var alert = $('#' + name + '-alert');
+    var size = $('#' + name + '-size');
+    submit.addEventListener(
+      'click',
+      function() {
+        var val = input.value;
+        if (val === '') {
+          toolsAlert(
+            name,
+            1500,
+            'Please insert your <strong>code</strong> first!'
+          );
+        }
+        if (!options.allowEmpty) {
+          if (!val.length) {
+            return;
+          }
+        }
+        alert.style.display = 'none';
+        try {
+          if (options.asyncResult) {
+            fn(val, options.asyncResult);
+          } else {
+            var result = fn(val, options.asyncResult);
+            var output = (input.value = result);
+            output;
+            if (size) {
+              toolsSize(name, val, output);
+            }
+          }
+        } catch (err) {
+          if (options.exception) {
+            options.exception(err);
+          }
+          return;
+        }
+        arr.push(val);
+        submit.disabled = true;
+        undo.style.display = 'inline-block';
+      },
+      false
+    );
+    toolsUndo(name, submit);
+    toolsClear(name, input, submit);
+    $('.tools-copy').addEventListener(
+      'click',
+      function() {
+        input.select();
+        if (!doc.execCommand) {
+          return;
+        }
+        doc.execCommand('copy');
+        if (supportsSmoothScroll) {
+          toolsScrollTo(input);
+        }
+      },
+      false
+    );
+    toolsOptions(name);
+  }
 
   // Public APIs
   win.tools = tools;
